b3cc4e165b6df82d7e4623953a6bccc913479ed1
Yunseong <dev.yunseong@gmail.com>
2025-08-19 16:53:43 +0900

    feature main controller

--- src/main/java/com/yunseong/website/controller/MainController.java
+++ src/main/java/com/yunseong/website/controller/MainController.java
@@ -3,14 +3,13 @@ package com.yunseong.website.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("")
     public String main() {
-        return "OK";
+        return "index";
     }
 }
