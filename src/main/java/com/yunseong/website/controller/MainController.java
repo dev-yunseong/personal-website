b64ea4b7c8fc9d250a7679bf3378de0874d7dfb7
Yunseong <dev.yunseong@gmail.com>
2025-08-19 16:46:57 +0900

    feature main controller

---
+++ src/main/java/com/yunseong/website/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.yunseong.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class MainController {
+
+    @GetMapping("")
+    public String main() {
+        return "OK";
+    }
+}
