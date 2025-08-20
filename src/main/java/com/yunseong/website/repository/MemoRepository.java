60d1fd6f19e5fe93988a88bd91a98adedabebfae
Yunseong <dev.yunseong@gmail.com>
2025-08-20 20:08:43 +0900

    feature database를 사용하도록 구현

---
+++ src/main/java/com/yunseong/website/repository/MemoRepository.java
@@ -0,0 +1,10 @@
+package com.yunseong.website.repository;
+
+import com.yunseong.website.domain.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    Optional<Memo> findByName(String name);
+}
