c1b4c5227396f7c13946bab61c6fdc9864542ebc
Yunseong <dev.yunseong@gmail.com>
2025-08-20 22:59:31 +0900

    feature: add transactional support to MemoService methods

--- src/main/java/com/yunseong/website/service/MemoService.java
+++ src/main/java/com/yunseong/website/service/MemoService.java
@@ -4,10 +4,12 @@ import com.yunseong.website.domain.Memo;
 import com.yunseong.website.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemoService {
 
@@ -19,15 +21,18 @@ public class MemoService {
         return memo.getId();
     }
 
+    @Transactional(readOnly = true)
     public Memo getMemo(String name) {
         return memoRepository.findByName(name)
                 .orElseThrow(() -> new IllegalArgumentException("Memo Not Found"));
     }
 
+    @Transactional(readOnly = true)
     public List<Memo> getMemos() {
         return memoRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Memo getMemo(long memoId) {
         return memoRepository.findById(memoId)
                 .orElseThrow(() -> new IllegalArgumentException("Memo Not Found"));
