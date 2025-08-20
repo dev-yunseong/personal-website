14ec18f29cf837beb62bb599e775e15ffd5d1e5a
Yunseong <dev.yunseong@gmail.com>
2025-08-20 23:23:01 +0900

    feature: update application configuration for security and simplify login setup

--- src/main/java/com/yunseong/website/config/WebSecurityConfig.java
+++ src/main/java/com/yunseong/website/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +21,7 @@ public class WebSecurityConfig {
                         .permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+                .formLogin(withDefaults());;
 
         return http.build();
     }
