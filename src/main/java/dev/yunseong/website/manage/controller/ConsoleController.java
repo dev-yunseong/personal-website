ade55e83375650ba6dee894a02049521565638b6
Yunseong <dev.yunseong@gmail.com>
2026-03-10 21:38:49 +0900

    feat(console): add Pageable support for improved pagination in ConsoleController

--- src/main/java/dev/yunseong/website/manage/controller/ConsoleController.java
+++ src/main/java/dev/yunseong/website/manage/controller/ConsoleController.java
@@ -6,6 +6,7 @@ import dev.yunseong.website.manage.service.RequestStatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
